8656a68338e65a94b74fa8ab74aef849c39487cb
BiancaDSantos <biancadossantossc@gmail.com>
2024-07-11 17:06:38 -0300

    Completando todas as funcionalidades

--- src/js/carregar.js
+++ src/js/carregar.js
@@ -1,14 +1,27 @@
 $(document).ready(function() {
+
+    const nomeBonitoParaOTipo = (tipo) => {
+        switch (tipo) {
+            case 'PREINCUBADORA': return 'Pré-Incubadora'
+            case 'INCUBADORA': return 'Incubadora'
+            case 'ACELERADORA': return 'Aceleradora'
+            default: return 'Not Found'
+        }
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const tipo = urlParams.get('tipo')
+
     axios
         .get("http://127.0.0.1:8080/agente")
         .then((response) => {
             if (response.status === 200) {
-                const lista = response.data;
+                const lista = response.data.filter(agente => tipo == null || agente.tipo == tipo);
                 for (const agente of lista) {
                     let linha = $("<tr/>");
                     linha.append($("<td/>").html(agente.nome));
                     linha.append($("<td/>").html(agente.descricao));
-                    linha.append($("<td/>").html(agente.tipo));
+                    linha.append($("<td/>").html(nomeBonitoParaOTipo(agente.tipo)));
                     linha.append($("<td style=\"white-space: pre-line\"/>")
                         .html(`Telefone: ${agente.telefone}\n Email: ${agente.email}`));
                     linha.append($("<td style=\"white-space: pre-line\"/>").html(`Cidade: ${agente.cidade?.nome ?? ""} \nLogradouro: ${agente.logradouro} \nNumero: ${agente.numero} \nCep: ${agente.cep} \nBairro: ${agente.bairro} \nComplemento: ${agente.complemento?? "Nada consta."}`));
@@ -20,16 +33,4 @@ $(document).ready(function() {
         .catch((err) => {
             alert(`Erro não Mapeado: ${err.message}`)
         });
-
-    // // Adicionar evento de clique nas linhas da tabela
-    // $(document).on('click', '#tabelaAgentes tr', function(event) {
-    //     const agenteId = $(this).data('id');
-
-    //     // Verificar se o ícone de edição foi clicado
-    //     if ($(event.target).hasClass('fa-pen-to-square')) {
-    //         window.location.href = `/main/admin/atualizado/atualizar.html?agenteId=${agenteId}`;
-    //     } else if (agenteId) {
-    //         window.location.href = `/produtos/registro/movimentacao.html?agenteId=${agenteId}`;
-    //     }
-    // });
 });
--- src/js/carregarAdmin.js
+++ src/js/carregarAdmin.js
@@ -1,43 +1,130 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    const nomeBonitoParaOTipo = (tipo) => {
+        switch (tipo) {
+            case 'PREINCUBADORA': return 'Pré-Incubadora'
+            case 'INCUBADORA': return 'Incubadora'
+            case 'ACELERADORA': return 'Aceleradora'
+            default: return 'Not Found'
+        }
+    }
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const tipo = urlParams.get('tipo')
+
     axios
         .get("http://127.0.0.1:8080/agente")
         .then((response) => {
             if (response.status === 200) {
-                const lista = response.data;
+                const lista = response.data.filter(agente => tipo == null || agente.tipo == tipo);
                 for (const agente of lista) {
                     let linha = $("<tr/>");
                     linha.append($("<td/>").html(agente.nome));
                     linha.append($("<td/>").html(agente.descricao));
-                    linha.append($("<td/>").html(agente.tipo));
-                    linha.append($("<td style=\"white-space: pre-line\"/>")
-                        .html(`Telefone: ${agente.telefone}\n Email: ${agente.email}`));
-                    linha.append($("<td style=\"white-space: pre-line\"/>").html(`Cidade: ${agente.cidade?.nome ?? ""} \nLogradouro: ${agente.logradouro} \nNumero: ${agente.numero} \nCep: ${agente.cep} \nBairro: ${agente.bairro} \nComplemento: ${agente.complemento}`));
-                    linha.append($("<td>").html("<i class='fa-solid fa-xmark' style='color: #fd0d0d;'></i><i class='fa-solid fa-pen-to-square' style='color: #3dff6e; margin-left: 10px;'></i>"));
-                    linha.attr('data-id', agente.id);
+                    linha.append($("<td/>").html(nomeBonitoParaOTipo(agente.tipo)));
+                    linha.append(
+                        $('<td style="white-space: pre-line"/>').html(
+                            `Telefone: ${agente.telefone}\n Email: ${agente.email}`
+                        )
+                    );
+                    linha.append(
+                        $('<td style="white-space: pre-line"/>').html(
+                            `Cidade: ${
+                                agente.cidade?.nome ?? ""
+                            } \nLogradouro: ${agente.logradouro} \nNumero: ${
+                                agente.numero
+                            } \nCep: ${agente.cep} \nBairro: ${
+                                agente.bairro
+                            } \nComplemento: ${agente.complemento}`
+                        )
+                    );
+                    linha.append(
+                        $("<td>").html(
+                            "<i class='fa-solid fa-xmark' style='color: #fd0d0d;'></i><i class='fa-solid fa-pen-to-square' style='color: #3dff6e; margin-left: 10px;'></i>"
+                        )
+                    );
+                    linha.attr("data-id", agente.id);
+                    linha.attr("data-nome", agente.nome);
                     $(tabelaAgentes).append(linha);
                 }
             }
         })
         .catch((err) => {
-            alert(`Erro não Mapeado: ${err.message}`)
+            alert(`Erro não Mapeado: ${err.message}`);
         });
 
     // Adicionar evento de clique nas linhas da tabela
-    $(document).on('click', '#tabelaAgentes tr', function(event) {
-        const agenteId = $(this).data('id');
+    $(document).on("click", "#tabelaAgentes tr", function (event) {
+        const agenteId = $(this).data("id");
+
+        const preencheDadosDoAgente = (agenteId, modal) => {
+            axios
+            .get("http://127.0.0.1:8080/agente/" + agenteId)
+            .then((response) => {
+                if (response.status === 200) {
+                    const agente = response.data;
+                    console.log(agente)
+                    let inputs = modal.querySelectorAll("input, select")
+                    for (let input of inputs) {
+                        if (input.id == "cidade") {
+                            for (let opcao of input.childNodes) {
+                                if (opcao.id == agente.cidade.id) opcao.selected = true
+                            }
+                        } else if (input.id == "tipo") {
+                            console.log(input.id)
+                            for (let opcao of input.childNodes) {
+                                console.log(opcao)
+                                if (opcao.id == agente.tipo) opcao.selected = true
+                            }
+                        } else {
+                            input.value = agente[input.id]
+                        }
+                    }
+                }
+            })
+            .catch((err) => {
+                alert(`Erro não Mapeado: ${err.message}`);
+            });
+        }
 
         // Verificar se o ícone de edição foi clicado
-        if ($(event.target).hasClass('fa-pen-to-square')) {
+        if ($(event.target).hasClass("fa-pen-to-square")) {
             // window.location.href = `/main/admin/atualizado/atualizar.html?agenteId=${agenteId}`;
-            const modal = document.getElementById("modal")
-            modal.style.display = "block"
-            
-            window.onclick = function(event) {
+            const modal = document.getElementById("modal");
+
+            preencheCidadesNoSelect(modal);
+
+            preencheDadosDoAgente(agenteId, modal)
+
+            window.onclick = function (event) {
                 if (event.target == modal) {
                     modal.style.display = "none";
                 }
-            }
-        } 
+            };
+
+            modal.style.display = "block";
+        }
+
+        if ($(event.target).hasClass("fa-xmark")) {
+            const modal = document.getElementById("modalExcluirAgente");
+
+            const elementoDeInformacao = document.getElementById("informacao-agente-excluir");
+
+            elementoDeInformacao.textContent = $(this).data("nome");
+
+            const botaoConfirma = document.getElementById("confirma-excluisao");
+
+            botaoConfirma.onclick = () => {excluiAgente(agenteId)}
+
+            window.onclick = function (event) {
+                if (event.target == modal) {
+                    modal.style.display = "none";
+                }
+            };
+
+            modal.style.display = "block";
+        }
+
         // else if (agenteId) {
         //     window.location.href = `/produtos/registro/movimentacao.html?agenteId=${agenteId}`;
         // }
